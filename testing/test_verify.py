90acaa013791384488f66193a1a1a90e1a1ca9af
Armin Rigo <arigo@tunes.org>
2012-05-31 21:43:28 +0200

    callbacks.

---
+++ testing/test_verify.py
@@ -0,0 +1,14 @@
+
+import py
+from ffi import FFI
+from platformer import CompilationError
+
+def test_simple_verify():
+    ffi = FFI()
+    ffi.cdef("void some_completely_unknown_function();")
+    py.test.raises(CompilationError, ffi.verify)
+    ffi = FFI()
+    ffi.cdef("double sin(double x);")
+    # omission of math.h
+    py.test.raises(CompilationError, ffi.verify)
+    assert ffi.verify('#include <math.h>') is None
