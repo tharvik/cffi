00c488a466db26c2bb04c229a9ba41748f3182e3
Maciej Fijalkowski <fijall@gmail.com>
2012-06-18 12:40:52 +0200

    Added tag release-0.1 for changeset 48c2b9f912db

---
+++ testing/test_cdata.py
@@ -0,0 +1,28 @@
+import py
+from cffi import FFI
+
+class FakeBackend(object):
+
+    def nonstandard_integer_types(self):
+        return {}
+
+    def sizeof(self, name):
+        return 1
+
+    def load_library(self, path):
+        return "fake library"
+
+    def new_primitive_type(self, name):
+        return FakePrimitiveType(name)
+
+class FakePrimitiveType(object):
+
+    def __init__(self, cdecl):
+        self.cdecl = cdecl
+
+
+def test_typeof():
+    ffi = FFI(backend=FakeBackend())
+    clong = ffi.typeof("signed long int")
+    assert isinstance(clong, FakePrimitiveType)
+    assert clong.cdecl == 'long'
