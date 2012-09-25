67033d9df9b92f6f47731a86aed33c4878522f04
Armin Rigo <arigo@tunes.org>
2012-09-25 10:24:53 +0200

    Fix for PyPy: load the '.pypy-19.so' file even though find_module() tends to return the dummy '.py' file created by setuptools.

---
+++ testing/test_integration.py
@@ -0,0 +1,60 @@
+
+import py, os, sys
+import tempfile
+import subprocess
+
+class DummyLogger(object):
+    indent = 0
+    
+    def __getattr__(self, attr):
+        return lambda *args: None
+
+def create_venv():
+    tmpdir = tempfile.mkdtemp()
+    subprocess.call(['virtualenv', '-p', sys.executable, tmpdir])
+    return py.path.local(tmpdir)
+
+SNIPPET_DIR = py.path.local(__file__).join('..', 'snippets')
+
+def run_setup_and_program(dirname, venv_dir, python_snippet):
+    olddir = os.getcwd()
+    tmpdir2 = py.path.local(tempfile.mkdtemp()) # this is for python files
+    python_f = tmpdir2.join('x.py')
+    python_f.write(py.code.Source(python_snippet))
+    try:
+        os.chdir(str(SNIPPET_DIR.join(dirname)))
+        venv = venv_dir.join('bin/activate')
+        p = subprocess.Popen(['bash', '-c', '. %(venv)s && python setup.py '
+                              'install && python %(python_f)s' % locals()],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = p.communicate()
+        if p.returncode != 0:
+            print stdout
+            print stderr
+            raise Exception("crashed")
+    finally:
+        os.chdir(olddir)
+
+def test_infrastructure():
+    venv_dir = create_venv()
+    run_setup_and_program('infrastructure', venv_dir, '''
+    import snip_infrastructure
+    assert snip_infrastructure.func() == 42
+    ''')
+
+def test_basic_verify():
+    venv_dir = create_venv()
+    run_setup_and_program("basic_verify", venv_dir, '''
+    import snip_basic_verify
+    p = snip_basic_verify.C.getpwuid(0)
+    assert snip_basic_verify.ffi.string(p.pw_name) == "root"
+    ''')
+
+def test_setuptools_verify():
+    venv_dir = create_venv()
+    run_setup_and_program("setuptools_verify", venv_dir, '''
+    import snip_setuptools_verify
+    p = snip_setuptools_verify.C.getpwuid(0)
+    assert snip_setuptools_verify.ffi.string(p.pw_name) == "root"
+    ''')
+    
